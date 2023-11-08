10d28ae364011364326bfb02b4f84c99db6e7f40
Ali Raza <aliraza137725@gmail.com>
2023-11-08 16:31:27 +0500

    some changes are done

---
+++ src/screens/Home/home.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import homeStyle from "./homeStyle"
+import React from 'react'
+
+export default function home() {
+  return (
+    <View>
+      <Text>home</Text>
+    </View>
+  )
+}
